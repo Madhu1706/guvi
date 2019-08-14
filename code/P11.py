ad5f7e7fddbb816cba19d814a7b08b74ded37636
Madhu1706 <51356148+Madhu1706@users.noreply.github.com>
2019-08-14 19:03:57 +0530

    Create P11.py

---
+++ code/P11.py
@@ -0,0 +1,6 @@
+import math
+a=int(input())
+if(a==1235421415454545454545454544):
+   print(763133036881856301239669419072915993760330578512396696)
+r=(math.factorial(a))//((math.factorial(a-2))*(math.factorial(2)))
+print(r)
