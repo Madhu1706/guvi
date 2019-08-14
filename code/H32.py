ba085915bee5d6616dfaff6ae340b94f2ec97711
Madhu1706 <51356148+Madhu1706@users.noreply.github.com>
2019-08-14 19:32:47 +0530

    Update H32.py

--- code/H32.py
+++ code/H32.py
@@ -1,8 +1,8 @@
 n=int(input())
 a=[int(x) for x in input().split()]
 lst=[]
-for i in range(0,len(l),2):
-    lst.append(l[i])
+for i in range(0,len(a),2):
+    lst.append(a[i])
 if(len(lst)<=2):
     print(lst[0])
 else:
